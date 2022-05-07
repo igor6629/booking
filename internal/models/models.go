edd912fd8c794935db985cff2057b953ca878f90
igor6629 <nechepurenko6629@gmail.com>
2022-05-07 19:01:08 +0300

    Adding new fields

--- internal/models/models.go
+++ internal/models/models.go
@@ -9,7 +9,7 @@ type User struct {
 	FirstName   string
 	LastName    string
 	Email       string
-	Phone       string
+	Password    string
 	AccessLevel int
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
